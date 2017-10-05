63b66a76934326afbc05ba54c36a6d103ca6734d
johnduval <john.duval@mtbethelchristian.org>
2017-10-05 15:41:18 -0400

    Added Autonmous Branch

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Aut_Red1.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+/**
+ * Created by johnduval on 10/5/17.
+ */
+
+public class Aut_Red1 extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        ColorSensor colorSensor;
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,16 +1,39 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+
 /**
  * Created by blake_shafer on 10/2/17.
  */
 
-public class Robot { /*
+public class Robot {
 
-    motor1 = whatever.get(motor)
+    DcMotor frontLeftMotor;
+    DcMotor frontRightMotor;
+    DcMotor rearLeftMotor;
+    DcMotor rearRightMotor;
+
+    double leftStickVal;
+    double rightStickVal;
+
+    double leftTriggerVal;
+    double rightTriggerVal;
+
+    double frontLeftSpeed;
+    double frontRightSpeed;
+    double rearLeftSpeed;
+    double rearRightSpeed;
+
+    //double speedLimiterFactor = 0.8;
+    //motor1 = whatever.get(motor)
 
 
     int SPD;
-
+/*
     Arm myArm = new arm(hardware);
     mechDrive myDrive = new mechDrive(hardware);
 
@@ -26,7 +49,7 @@ public class Robot { /*
         myDrive.back();
 
 
-    } */
-
+    }
+*/
 
 }
