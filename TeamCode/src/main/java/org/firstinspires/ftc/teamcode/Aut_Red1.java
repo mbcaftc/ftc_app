6741025ec4e0f1c7747ac65dd8b553b3095da80e
johnduval <john.duval@mtbethelchristian.org>
2017-10-05 15:59:51 -0400

    Red Aut 1 creation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Aut_Red1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Aut_Red1.java
@@ -1,15 +1,95 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.app.Activity;
+import android.view.View;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by johnduval on 10/5/17.
  */
 
+@Autonomous (name = "Red - 1", group = "RED")
+
 public class Aut_Red1 extends LinearOpMode {
+    ColorSensor colorSensor;
+    Servo colorSensorArm;
+    double upPosition = 0.325;
+    double downPosition = 1.0;
+
+    boolean armState; // Up = false, down = true
+
+    DcMotor frontLeftMotor;
+    DcMotor frontRightMotor;
+    DcMotor rearLeftMotor;
+    DcMotor rearRightMotor;
+
+    double frontLeftSpeed;
+    double frontRightSpeed;
+    double rearLeftSpeed;
+    double rearRightSpeed;
+
+    double speedLimiterFactor = 0.8;
+
     @Override
     public void runOpMode() throws InterruptedException {
-        ColorSensor colorSensor;
+
+        colorSensorArm = hardwareMap.servo.get("color_sensor_arm");
+        colorSensorArm.setPosition(upPosition); // Set to up position
+
+        frontLeftMotor = hardwareMap.dcMotor.get("front_left_motor");
+        frontRightMotor = hardwareMap.dcMotor.get("front_right_motor");
+        rearLeftMotor = hardwareMap.dcMotor.get("rear_left_motor");
+        rearRightMotor = hardwareMap.dcMotor.get("rear_right_motor");
+
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        rearLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rearLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rearRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rearLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rearRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // hsvValues is an array that will hold the hue, saturation, and value information.
+        float hsvValues[] = {0F,0F,0F};
+
+        // values is a reference to the hsvValues array.
+        final float values[] = hsvValues;
+
+        // get a reference to the RelativeLayout so we can change the background
+        // color of the Robot Controller app to match the hue detected by the RGB sensor.
+        final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(com.qualcomm.ftcrobotcontroller.R.id.RelativeLayout);
+
+        // bPrevState and bCurrState represent the previous and current state of the button.
+        boolean bPrevState = false;
+        boolean bCurrState = false;
+
+        // bLedOn represents the state of the LED.
+        boolean bLedOn = false;
+
+        // get a reference to our ColorSensor object.
+        colorSensor = hardwareMap.colorSensor.get("sensor_color");
+
+        // Set the LED in the beginning
+        colorSensor.enableLed(bLedOn);
+
+
+        waitForStart();
+
+
+        telemetry.addData("Servo", "Position: " + String.format("%.3f", colorSensorArm.getPosition()));
+        telemetry.addData("BLUE: ", colorSensor.blue());
+        telemetry.addData("RED: ", colorSensor.red());
+        telemetry.update();
+
     }
 }
